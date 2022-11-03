c3d3a327fd25ad73a462f669818d847f6a024f1e
zhuscat <zhudx6512@163.com>
2022-11-03 18:48:25 +0800

    feat: add js lsp

--- lua/plugin-config/mason-lspconfig.lua
+++ lua/plugin-config/mason-lspconfig.lua
@@ -1,5 +1,7 @@
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "rust_analyzer" }
+  ensure_installed = { "sumneko_lua", "tsserver", "volar", "rust_analyzer" }
 })
 
 require('lspconfig').sumneko_lua.setup {}
+require('lspconfig').tsserver.setup {}
+require('lspconfig').volar.setup {}
